1a5541b8e1f08b8755be861b7bf36cd68cdf24a5
amjad-desai <amjad@synclarity.in>
2018-03-03 19:00:40 +0400

    minor tweaks

--- client/main.js
+++ client/main.js
@@ -1,22 +1,18 @@
-//import libraries
-
+//IMPORT LIBRARIES
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-// React component is a function in react that
-// generates some amount of html code in our app
+/* React component is a function in react that generates some amount of html code in our app */
 
 const App = () => {
-  return <div>hello </div>;
+  return <div>This is a Meteor Boilerplate </div>;
 };
-//create component
 
-//render the react component
+//CREATE COMPONENT
 
+//RENDER COMPONENT
+
+/*react render call */
 Meteor.startup(() => {
-  //react render call
   ReactDOM.render(<App />, document.querySelector('.container'));
 });
-// <app /> creates a instance of an a App and put it in an html with class .container
-//Metero.startup arrouw head function make sure everything is loaded before
-// react code runs.
